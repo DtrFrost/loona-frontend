9422bee663d15588e51ca5226fca972acb8e412d
DtrFrost <msityaev0708@gmail.com>
2026-05-21 18:19:48 +0300

    URL's changed

--- frontend/src/services/fileService.js
+++ frontend/src/services/fileService.js
@@ -1,4 +1,4 @@
-import API_BASE_URL from '../../config/api'
+import API_BASE_URL from '../config/api';
 
 const getToken = () => {
     const token = localStorage.getItem('token') || sessionStorage.getItem('token');
